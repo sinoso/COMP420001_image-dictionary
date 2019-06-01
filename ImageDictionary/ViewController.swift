6fde4380b3843085ff1d2206adba692d87abab72
14 김세현 <14kimsehyeon@knuprime026ui-MacBookPro.local>
2019-06-02 04:04:50 +0900

    reset

---
+++ ImageDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageDictionary
+//
+//  Created by 14 김세현 on 31/05/2019.
+//  Copyright © 2019 comp420. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
