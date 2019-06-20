e83c4907bb0007f1a627432b68538e6e11ad6b0c
soon-woo-kwon <fkausrndanf@naver.com>
2019-06-20 17:00:22 +0900

    note add

--- ImageDictionary/Record.swift
+++ ImageDictionary/Record.swift
@@ -10,20 +10,19 @@ class Record: UIViewController {
     override func viewDidLoad() {//View가 로드된 후에 나오는 것들
         super.viewDidLoad()
 
-        MemoData = UserDefaults.standard.object(forKey: "MemoData") as? [String] ?? [String]()
-        
-        if MemoData.count == 0 {
+        let MemoNumber = UserDefaults.standard.object(forKey: "MemoNumber") as! Int
+        if MemoNumber == -1 {
+            MemoData = UserDefaults.standard.object(forKey: "MemoData") as! [String]
             
-            RecordTextView.text = "..."
+                RecordTextView.text = "..."
             
         } else {
+            MemoData = UserDefaults.standard.object(forKey: "MemoData") as! [String]
             
-        let MemoNumber = UserDefaults.standard.object(forKey: "MemoNumber") as! Int
-            
-        RecordTextView.text = MemoData[MemoNumber]
-            
+            RecordTextView.text = MemoData[MemoNumber]
         }
     }
+    
     
 
     @IBAction func Save(_ sender: Any) {
@@ -33,7 +32,7 @@ class Record: UIViewController {
         UserDefaults.standard.set(MemoData, forKey: "MemoData")
         } else {
             MemoData.remove(at: MemoNumber)
-            MemoData.insert(RecordTextView.text, at: MemoNumber)
+            MemoData.insert(RecordTextView.text , at: MemoNumber)
             
             UserDefaults.standard.set(MemoData, forKey: "MemoData")
         }
